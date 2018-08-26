e64d504358fdccf1ad097d6519720990e28a6f27
tsukisawa-yuya <mh21sus@gmail.com>
2018-08-26 11:59:50 +0900

    add customers controller

---
+++ db/migrate/20180826024708_create_customers.rb
@@ -0,0 +1,12 @@
+class CreateCustomers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :customers do |t|
+      t.string :family_name,null: false, length:{ maximam: 20}
+      t.string :given_name, null: false, length: { maximum: 20 }
+      t.string :email,      null: false
+
+      t.timestamps
+    end
+    add_index :customers, :email, unique: true
+  end
+end
